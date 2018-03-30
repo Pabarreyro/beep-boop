cd1ef9f54a04396ea7c21f0a404ed162802cee4c
Pablo Barreyro <Pabarreyro@gmail.com>
2018-03-30 09:24:05 -0700

    add a function for replacing numbers containing 0 in an array with 'Beep' to backend (Spec 2)

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,25 @@ function findRange(number) {
   var rangeArray = [];
 
   for (var i = 0; i <= number; i++) {
-    rangeArray.push(i);
+    rangeArray.push(i.toString());
   }
   return rangeArray;
-}
+};
+
+function replaceNumbers(array) {
+  newArray = array.map(function(element){
+    if (element.match(/0/)) {
+      return "<em>Beep!</em>";
+    } else {
+      return element;
+    }
+  });
+
+  return newArray;
+};
+
+testNumber = 10;
+testArray = findRange(testNumber);
+console.log(testArray);
+testArrayReplace0 = replaceNumbers(testArray);
+console.log(testArrayReplace0);
